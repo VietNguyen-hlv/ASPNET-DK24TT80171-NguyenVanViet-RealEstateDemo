567c8d115d0747fc356e6c4561b4e5a0d2e27b1c
Việt Nguyễn <73325557+VietNguyen-hlv@users.noreply.github.com>
2026-06-12 21:33:05 +0700

    Add files via upload

---
+++ src/RealEstateWebsite/Program.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using RealEstateWebsite.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+    });
+
+builder.Services.AddAuthentication(options =>
+{
+   
+    options.DefaultScheme = "CookieAuth";
+    options.DefaultChallengeScheme = "CookieAuth";
+})
+.AddCookie("CookieAuth", options =>
+{
+    
+    options.LoginPath = "/Account/Login";
+    options.LogoutPath = "/Account/Logout";
+})
+.AddCookie("AdminCookie", options =>
+{
+   
+    options.LoginPath = "/admin/login";
+    options.LogoutPath = "/admin/logout";
+    options.AccessDeniedPath = "/admin/login";
+});
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+
+var app = builder.Build();
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+   
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
+);
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=batdongsan}/{action=Index}/{id?}"
+);
+
+app.Run();
